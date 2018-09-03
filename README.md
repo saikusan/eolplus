bb73c61e9ab342d9a2f443f32d46f4caed94a827
Claudio <claudio.ilea@gmail.com>
2018-09-03 10:58:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# EOL+
+
+Este script está pensado para añadir mejoras y nuevas funciones a [ElOtroLado.net], entre las que se incluyen:
+
+## Instalación
+
+Para poder usar este script, es necesario tener instalado previamente [Greasemonkey], [Tampermonkey] u otro [compatible][1] dependiendo de tu navegador.
+
+Para instalar el script, haz click aquí: [Instalar][2]
+
+[1]: https://github.com/saikusan/eolplus/wiki/Instalaci%C3%B3n
+[2]: https://raw.githubusercontent.com/saikusan/eolplus/master/eolplus.user.js
+[ElOtroLado.net]: https://www.elotrolado.net/
+[Greasemonkey]: https://addons.mozilla.org/es/firefox/addon/greasemonkey/
+[Tampermonkey]: https://tampermonkey.net/?ext=dhdg&browser=chrome
---
+++ eolplus.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         EOL+
+// @namespace    EOL+
+// @description  Mejoras y nuevas funciones para ElOtroLado.net.
+// @author       Saikuro
+// @copyright    2018+, Saikuro
+// @version      0.0.1
+// @license      MIT
+// @homepageURL  https://github.com/saikusan/eolplus
+// @supportURL   https://github.com/saikusan/eolplus/issues
+// @updateURL    https://raw.githubusercontent.com/saikusan/eolplus/master/eolplus.meta.js
+// @downloadURL  https://raw.githubusercontent.com/saikusan/eolplus/master/eolplus.user.js
+// @icon         https://raw.githubusercontent.com/saikusan/eolplus/master/resources/img/eol.png
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.deleteValue
+// @run-at       document-end
+// @match        https://www.elotrolado.net/*
+// ==/UserScript==
